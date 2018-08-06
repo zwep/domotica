4cbda34d77934c9d0669f4defaf47d95c79ef97e
zwep <zwep718@gmail.com>
2018-08-06 14:52:20 +0200

    finalized call_rss

--- crontab_calls/hourly/call_rss.py
+++ crontab_calls/hourly/call_rss.py
@@ -1,85 +1,48 @@
 # encoding: utf-8
 
 """
-How to store the text data? Elastic DB?
-
-
-TODO test this thing
-TODO upload it to git..
-
-Layout of the folder/table structure
-
-/data
-    /text
-        /news (or social media..)
-            /nos
-                /date (YYYY-MM-DD)
-                    /morning
-                    /noon
-                    /afternoon
-            /...
+This is the script that can be run on a hourly basis in order to catch all the news items from various sources.
 """
 
 from zwep.helper import loadrss as rss
 
+import pytz
 import os
-import time
+from datetime import datetime
 from pathlib import Path
+from elasticsearch import Elasticsearch
 
+es = Elasticsearch()
+rss_obj = rss.RssUrl()
+res_text = rss_obj.get_all_content()
+news_source_list = rss_obj.news_source_list
 
-def create_path(path):
-    """
-    With this we want to check the directory..
-    :param path:
-    :return:
-    """
-    if not os.path.isdir(path):
-        Path(path).mkdir(parents=True, exist_ok=True)
-        print('\n Created path: ', path)
-    else:
-        print('\n Path already exists: ', path)
+update_file = 'last_update.txt'
+update_path = '/home/data'
+update_file_path = os.path.join(update_path, update_file)
 
-
-def check_duplicate(orig_data, new_data):
-    """
-    Here we will chekc if we already have the relveant info..
-    :param orig:
-    :param new:
-    :return:
-    """
-    # maybe sort the new data
-    title_text = [x['title'] for x in new_data]
-    orig_title_text = [x['title'] for x in orig_data]
-
-    for i, i_title in enumerate(title_text):
-        if i_title in orig_title_text:
-            print(i)
-
-
-PATH_TEXT_DATA = '/home/data/text'
-PATH_SPEC = os.path.join(PATH_TEXT_DATA, 'news')
-
-A = rss.RssUrl()
-res_text = A.get_all_content()
-news_source_list = A.news_source_list
-
-t_time = time.localtime()
-t_date = [str(t_time.tm_year), str(t_time.tm_mon).zfill(2), str(t_time.tm_mday).zfill(2)]
-t_hour = t_time.tm_hour
-
-if t_hour > 0 and t_hour < 12:
-    t_hour_class = 'morning'
-elif t_hour >= 12 and t_hour < 18:
-    t_hour_class = 'noon'
-elif t_hour >= 18 and t_hour < 24:
-    t_hour_class = 'evening'
+if os.path.isfile(update_file_path):
+    with open(update_file_path, 'r') as f:
+        last_update = f.read()
 else:
-    print('Something went wrong')
-    t_hour_class = 'unkown'
+    # Just make some fake earlier date..
+    last_update = '1999-12-31 01:01:01.01'
 
-news_path_list = {x: os.path.join(PATH_SPEC, x, '/'.join(t_date)) for x in news_source_list}
+last_update = datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S.%f')
+last_update = pytz.utc.localize(last_update)
 
-for news_source, news_path in news_path_list.items():
-    create_path(news_path)
-    news_source = 'nos'
-    res_text[news_source].keys()
+for news_source in news_source_list:
+    news_content = res_text[news_source]
+    i_content = news_content[0]
+    for i_content in news_content:
+        content_date = datetime.strptime(i_content['date'], '%a, %d %b %Y %H:%M:%S %z')
+
+        if content_date > last_update:
+            es.index(index='news', doc_type=news_source, body=i_content)
+        else:
+            print('We already have the content')
+
+
+current_time = str(datetime.now())
+with open(update_file_path, 'w') as f:
+    f.write(current_time)
