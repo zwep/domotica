ab1f4aaae2a961cdb16f041528c7642d5e28c48f
zwep <zwep718@gmail.com>
2018-12-02 21:48:08 +0100

    added markers

--- crontab_calls/hourly/netgear.py
+++ crontab_calls/hourly/netgear.py
@@ -2,22 +2,24 @@
 
 """ Testing netgear connection """
 
-print('Starting script')
+print('Starting script\n')
 from pynetgear import Netgear
 from elasticsearch import Elasticsearch
 import os
 from datetime import datetime
+print('Loaded all libraries\n')
 es = Elasticsearch()
-
+print('Setup elasticsearch \n')
 netgear = Netgear(password=os.environ['netgear_key'])
-print('We are logged in with username: ', netgear.username)
+print('Logged in with username: ', netgear.username)
 
+print('Searching for devices')
 for i in netgear.get_attached_devices():
     temp_dict = dict(zip(i._fields, list(i)))
     temp_dict['date'] = datetime.isoformat(datetime.now())
     es.index(index='netgear', body=temp_dict, doc_type='_doc')
 
-print('attached devices have been connected')
+print('Devices have been found')
 
 traffic_dict = netgear.get_traffic_meter()
 traffic_dict['date'] = datetime.isoformat(datetime.now())
