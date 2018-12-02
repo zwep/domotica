196c850424532e94b053176516d9109cd4908fc4
zwep <zwep718@gmail.com>
2018-12-02 21:25:01 +0100

    added words to netgear file

--- crontab_calls/hourly/netgear.py
+++ crontab_calls/hourly/netgear.py
@@ -9,15 +9,18 @@ from datetime import datetime
 es = Elasticsearch()
 
 netgear = Netgear(password=os.environ['netgear_key'])
+print('We are logged in with username: ', netgear.username)
 
 for i in netgear.get_attached_devices():
     temp_dict = dict(zip(i._fields, list(i)))
     temp_dict['date'] = datetime.isoformat(datetime.now())
     es.index(index='netgear', body=temp_dict, doc_type='_doc')
 
+print('attached devices have been connected')
+
 traffic_dict = netgear.get_traffic_meter()
 traffic_dict['date'] = datetime.isoformat(datetime.now())
 es.index(index='netgear', body=traffic_dict, doc_type='_doc')
 
-
+print('traffic meter as well')
 
