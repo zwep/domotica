4b499df9e1f67e63b38639a7875fdf72d4de05c2
zwep <zwep718@gmail.com>
2018-12-02 22:03:33 +0100

    environmental variables in cron are different

--- crontab_calls/hourly/netgear.py
+++ crontab_calls/hourly/netgear.py
@@ -9,25 +9,20 @@ import os
 from datetime import datetime
 print('Loaded all libraries\n')
 es = Elasticsearch()
-print('Setup elasticsearch \n')
 
-print('Environmental keys')
+
+
 for i in os.environ.keys():
     print(i)
 netgear = Netgear(password=os.environ['netgear_key'])
-print('Logged in with username: ', netgear.username)
 
-print('Searching for devices')
 for i in netgear.get_attached_devices():
     temp_dict = dict(zip(i._fields, list(i)))
     temp_dict['date'] = datetime.isoformat(datetime.now())
     es.index(index='netgear', body=temp_dict, doc_type='_doc')
 
-print('Devices have been found')
 
 traffic_dict = netgear.get_traffic_meter()
 traffic_dict['date'] = datetime.isoformat(datetime.now())
 es.index(index='netgear', body=traffic_dict, doc_type='_doc')
-
-print('traffic meter as well')
 
