5b67159cf4b54e03aed2a956e85b02213c9aee9e
henk <sebharrevelt@gmail.com>
2020-12-02 09:57:50 +0100

    updated on advent of cdoe

--- adventofcode_2020/day1.f90
+++ adventofcode_2020/day1.f90
@@ -1,10 +1,31 @@
+function bubblesort(a)
+     n = size(a)
+
+    !# Traverse through all array elements
+    do i = 1, n
+        !# Last i elements are already in place
+        do j = 1, n-i-1
+            !# traverse the array from 0 to n-i-1
+            !# Swap if the element found is greater
+            !# than the next element
+            if (a(j) > a(j+1)) then
+                arr(j) = arr(j+1)
+                arr(j+1) = arr(j)
+            end if
+        end do
+
+    end do
+
+end function
 program readfromfile
-    integer, dimension(200,1) :: cs
+    real, dimension(200) :: cs
 
     open(unit=15, file='./adventofcode_2020/day1_numbers.txt', status='old', action='read')
     read(15,*) cs
-    do i=1,200
-        print *, cs(i, 1)
+    call bubble_sort(cs)
+    do i=1, last
+        print *, cs(i)
     end do
+
 
 end program readfromfile
