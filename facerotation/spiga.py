e399ca7beed2cd10ceee468016e23e7eaeec4856
zwep <sebharrevelt@gmail.com>
2024-04-12 14:43:56 +0200

    update

--- facerotation/spiga.py
+++ facerotation/spiga.py
@@ -1,9 +1,12 @@
 import os.path
-
 import spiga.demo.analyze.extract.spiga_processor as pr_spiga
+import spiga.demo.analyze.extract.processor as pr
 
 spiga_dataset = '300wpublic'
-processor = pr_spiga.SPIGAProcessor(dataset=spiga_dataset)
+
+from facerotation.spiga_proc import SPIGAProcessor
+
+SPIGAProcessor(dataset=spiga_dataset)
 
 from retinaface import RetinaFace
 import matplotlib.pyplot as plt
